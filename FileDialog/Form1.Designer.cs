42466a322b6ba2a240af10a2112630a02a7664dd
Rifky-ahamed <rifkyr099@gmail.com>
2024-02-21 07:52:33 +0530

    Add project files.

---
+++ FileDialog/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FileDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtxtFileContent = new System.Windows.Forms.RichTextBox();
+            this.btnBrowse2 = new System.Windows.Forms.Button();
+            this.btnBrowse3 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(99, 25);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(270, 26);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(377, 22);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse 1";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Name:";
+            // 
+            // rtxtFileContent
+            // 
+            this.rtxtFileContent.Location = new System.Drawing.Point(13, 129);
+            this.rtxtFileContent.Name = "rtxtFileContent";
+            this.rtxtFileContent.Size = new System.Drawing.Size(465, 280);
+            this.rtxtFileContent.TabIndex = 3;
+            this.rtxtFileContent.Text = "";
+            // 
+            // btnBrowse2
+            // 
+            this.btnBrowse2.Location = new System.Drawing.Point(377, 58);
+            this.btnBrowse2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse2.Name = "btnBrowse2";
+            this.btnBrowse2.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse2.TabIndex = 4;
+            this.btnBrowse2.Text = "Browse 2";
+            this.btnBrowse2.UseVisualStyleBackColor = true;
+            this.btnBrowse2.Click += new System.EventHandler(this.btnBrowse2_Click);
+            // 
+            // btnBrowse3
+            // 
+            this.btnBrowse3.Location = new System.Drawing.Point(377, 94);
+            this.btnBrowse3.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse3.Name = "btnBrowse3";
+            this.btnBrowse3.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse3.TabIndex = 5;
+            this.btnBrowse3.Text = "Browse 3";
+            this.btnBrowse3.UseVisualStyleBackColor = true;
+            this.btnBrowse3.Click += new System.EventHandler(this.btnBrowse3_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(377, 418);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 7;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 459);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowse3);
+            this.Controls.Add(this.btnBrowse2);
+            this.Controls.Add(this.rtxtFileContent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFileName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "File Dialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtxtFileContent;
+        private System.Windows.Forms.Button btnBrowse2;
+        private System.Windows.Forms.Button btnBrowse3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FileDialog/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt)|*.txt|image (.jpeg)|*.JPEG|all files(*.*)|*.*" ;
+            ofd.FilterIndex = 2;
+            ofd.InitialDirectory = @"D:\";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = ofd.FileName;
+                string fileContent = File.ReadAllText(ofd.FileName);
+                rtxtFileContent.Text = fileContent;
+            }
+        }
+
+        private void btnBrowse2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt) | *.txt | All |*.*";
+            ofd.FilterIndex = 2;
+            ofd.InitialDirectory = @"D:\";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = ofd.FileName;
+                string [] fileContent = File.ReadAllLines(ofd.FileName);
+
+                textBox1.Text = fileContent.Length.ToString();
+
+               /* foreach (string con in fileContent)
+                {
+                    rtxtFileContent.Text += con + "\n";
+                } */
+                
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    rtxtFileContent.Text += fileContent[i]+"\n";
+                }
+                
+            }
+        }
+
+        private void btnBrowse3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt) | *.txt | All |*.*";
+            ofd.FilterIndex = 2;
+            ofd.InitialDirectory = @"D:\";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = ofd.FileName;
+                byte[] fileContent = File.ReadAllBytes(ofd.FileName);
+
+                foreach (byte b in fileContent)
+                {
+                    rtxtFileContent.Text += (char)b;
+                }
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = @"C:\Users\Nadeera\Documents\ATI\IT\HNDIT1012\Practical";
+            sfd.Filter = "Text Files (*.txt) | *.txt|image file (.jpeg)| *.JPEG";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, rtxtFileContent.Text);
+            }
+        }
+
+       
+    }
+}
